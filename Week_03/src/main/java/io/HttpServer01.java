18d0214bc95c84f074d0871a7a94eaa335726ced
zyc <zyc0122@126.com>
2021-01-29 16:00:24 +0800

    作业3

---
+++ Week_03/src/main/java/io/HttpServer01.java
@@ -0,0 +1,34 @@
+package io;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer01 {
+  public static void main(String[] args) {
+    try {
+      ServerSocket serverSocket = new ServerSocket(8801);
+      while(true) {
+        Socket socket = serverSocket.accept();
+        service(socket);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void service(Socket socket) {
+    try (PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true)) {
+      printWriter.println("HTTP/1.1 200 OK");
+      printWriter.println("Content-Type:text/html;charset=utf-8");
+      String str = "hello.nio";
+      printWriter.println("Content-Length:" + str.length());
+      printWriter.println();
+      printWriter.write(str);
+      socket.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
